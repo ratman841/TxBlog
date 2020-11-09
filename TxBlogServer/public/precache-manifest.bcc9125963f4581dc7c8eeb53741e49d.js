e14eb813bc64415bfaf922d6916bf54b467bcde1
kaeser <kaeser@mivune.com>
2020-11-09 14:29:58 +0100

    added client static files to /public, added dockerfile

---
+++ TxBlogServer/public/precache-manifest.bcc9125963f4581dc7c8eeb53741e49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11accd1a8ec3d4a98900377db1a62ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2af14b9ccd8d89b6493c",
+    "url": "./static/js/2.0cbd9f17.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.0cbd9f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4792901f8bd53774dfd0",
+    "url": "./static/js/main.201c7dae.chunk.js"
+  },
+  {
+    "revision": "b3f71f17eb1e13cef838",
+    "url": "./static/js/runtime-main.252b23d9.js"
+  }
+]);
