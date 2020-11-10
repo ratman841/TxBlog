8ace204524bbbee9f641f97cc07d2825502edab7
kaeser <kaeser@mivune.com>
2020-11-10 09:08:08 +0100

    updated client build version

---
+++ TxBlogClient/build/precache-manifest.b24f12786d36c015d2dfe601ba7ec284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc2f8bb3d41de01195b8d59c7c94c67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2af14b9ccd8d89b6493c",
+    "url": "./static/js/2.0cbd9f17.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.0cbd9f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7526a98cda94f1f17a5c",
+    "url": "./static/js/main.1576e14e.chunk.js"
+  },
+  {
+    "revision": "b3f71f17eb1e13cef838",
+    "url": "./static/js/runtime-main.252b23d9.js"
+  }
+]);
